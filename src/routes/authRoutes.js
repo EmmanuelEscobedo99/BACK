08b318468a574a5dadf863ca6713f02f3d4edd12
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-02-03 15:51:02 -0600

    Creacion y configuracion de la base de datos con MongoDB, primer modelo y primeras rutas sin logica

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.post('/register', (req, res) => {
+    console.log('/register Hiciste una peticion POST a /register')
+    res.json({ message: 'Hiciste una peticion POST a /register' })
+})
+
+router.post('/login', (req, res) => {
+    console.log('/register Hiciste una peticion POST a /login')
+    res.json({ message: 'Hiciste una peticion POST a /login' })
+})
+
+router.post('/logout', (req, res) => {
+    console.log('/register Hiciste una peticion POST a /logout')
+    res.json({ message: 'Hiciste una peticion POST a /logout' })
+})
+
+router.get('/profile', (req, res) => {
+    console.log('/register Hiciste una peticion GET a /profile')
+    res.json({ message: 'Hiciste una peticion GET a /profile' })
+})
+
+export default router
