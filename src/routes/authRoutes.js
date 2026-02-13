969add3347f968d9e91ff1082550bde4752714df
EmmanuelEscobedo99 <emmanuelescobedo099@gmail.com>
2026-02-12 18:52:00 -0600

    ya loguea usuarios

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -11,11 +11,6 @@ router.post('/register', registerUser)
 
 router.post('/login', loginUser)
 
-router.post('/login', (req, res) => {
-    console.log('/register Hiciste una peticion POST a /login')
-    res.json({ message: 'Hiciste una peticion POST a /login' })
-})
-
 router.post('/logout', (req, res) => {
     console.log('/register Hiciste una peticion POST a /logout')
     res.json({ message: 'Hiciste una peticion POST a /logout' })
